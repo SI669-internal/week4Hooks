32bf7dd3e7b81e6007fd9dfa4be897d2754047a1
Mark Newman <mwnewman@umich.edu>
2022-09-19 23:12:17 -0400

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+function App1() {
+  let [greeting, setGreeting] = useState('Hello');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{greeting}</Text>
     </View>
   );
 }
 
+class App2 extends React.Component {
+  constructor() {
+    super();
+    this.state = { greeting: 'Hello '}
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.greeting}</Text>
+      </View>
+    );
+  }
+}
+
+function App3() {
+  let [greeting, setGreeting] = useState("Hello");
+  let [counter, setCounter] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text>{greeting}</Text>
+      <Text>You've pressed the button {counter} times!</Text>
+      <Button
+        title="More!"
+        onPress={()=>{
+          setCounter(counter + 1);
+        }}
+      />
+    </View>
+  );
+}
+
+function App4() {
+  let [greeting, setGreeting] = useState("Hello");
+  let [counter, setCounter] = useState(100);
+
+  return (
+    <View style={styles.container}>
+      <Text>{greeting}</Text>
+      <Text>You've pressed the button {counter} times!</Text>
+      <Button
+        title="More!"
+        onPress={()=>{
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Less!"
+        onPress={()=>{
+          setCounter(counter - 1);
+        }}
+      />
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +74,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+//export default App1;
+//export default App2;
+//export default App3;
+export default App4;
+
